6db9466d370106892b1b2993a8589955b1892884
karthi281104 <karthikeyana281104@gmail.com>
2025-03-09 18:42:53 +0530

    hello

---
+++ frontend/src/animals.js
@@ -0,0 +1,161 @@
+import React from 'react';
+
+  function AnimalPage() {
+    
+    return (
+
+
+
+
+    <div id="sectionsecond">
+        <div class="second_bg_color">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+
+
+
+                        <h1 class="upcomming_event block"> Choose what type of animals you want to buy :</h1>
+                        <div class="container">
+                            <div class="row">
+
+
+                                <div class="col-12 col-lg-4 col-xl-3">
+                                    <div class="event_card shadow-lg mb-4">
+                                        <h1 class="event_head block">Cows</h1>
+
+                                        <div class="carosel block">
+                                            <div id="carouselExampleIndicators " class="carousel slide" data-ride="carousel">
+                                                <ol class="carousel-indicators">
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                                </ol>
+                                                <div class="carousel-inner carosel_edit">
+                                                    <div class="carousel-item">
+                                                        <img src="https://i.pinimg.com/originals/fa/8b/0a/fa8b0a1810433af595b81e10b89c0821.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                    <div class="carousel-item active">
+                                                        <img src="https://blog.statiq.in/wp-content/uploads/2023/04/image-12-1280x600.png" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                    
+                                                    <div class="carousel-item">
+                                                        <img src="https://i.ndtvimg.com/i/2015-12/10-best-selling-cars_827x510_41451295973.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+
+
+                        
+                                        <button class="info_button mt-1 block" onclick="display('sectionsmartindiahackdetails')">Info more</button>
+                                    </div>
+                                </div>
+
+
+
+
+
+
+
+
+
+                                
+
+
+
+                                <div class="col-12 col-lg-4 col-xl-3">
+                                    <div class="event_card shadow-lg mb-4">
+                                        <h1 class="event_head block">Goats</h1>
+
+                                        <div class="carosel block">
+                                            <div id="carouselExampleIndicators " class="carousel slide" data-ride="carousel">
+                                                <ol class="carousel-indicators">
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                                </ol>
+                                                <div class="carousel-inner carosel_edit">
+                                                    <div class="carousel-item active">
+                                                        <img src="https://www.outdoorhappens.com/wp-content/uploads/2023/02/adult-goat-on-green-grass-forage-enjoying-a-free-range-lifestyle.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <img src="https://i1.wp.com/www.asphaltandrubber.com/wp-content/uploads/2014/07/Energica-Ego-electric-superbike-launch-Scott-Jones-26.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <img src="https://1.bp.blogspot.com/-3UGMrO4GC3M/XUgt2FfgkXI/AAAAAAAAAGE/iT5wgisE_YYKQCNscD7MGfbN8no7N5_WgCLcBGAs/s1600/most-expensive-Bike-of-the-world-e1562675381880.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+
+                                        <button class="info_button mt-1 block">Info more</button>
+                                    </div>
+                                </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                <div class="col-12 col-lg-4 col-xl-3">
+                                    <div class="event_card shadow-lg mb-4">
+                                        <h1 class="event_head block">Dogs</h1>
+
+                                        <div class="carosel block">
+                                            <div id="carouselExampleIndicators " class="carousel slide" data-ride="carousel">
+                                                <ol class="carousel-indicators">
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                                                </ol>
+                                                <div class="carousel-inner carosel_edit">
+                                                    <div class="carousel-item active">
+                                                        <img src="https://media.tag24.de/951x634/d/n/dnw3jj8a65uvjbpzz7t7s74s4jdeuyyr.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <img src="https://1.bp.blogspot.com/-FRUSU3frfew/Tt8F0k9EEuI/AAAAAAAAABM/BJTfSvy4pMk/s1600/L1.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                    <div class="carousel-item">
+                                                        <img src="https://a1digitalcampus.at/wp-content/uploads/2022/09/Girls-hackaton-2022-day1-JH-67.jpg" class="d-block w-100" alt="..." />
+                                                    </div>
+                                                </div>
+                                                
+                                            </div>
+                                        </div>
+
+                                        <button class="info_button mt-1 block">Info more</button>
+                                    </div>
+                                </div>
+
+
+
+
+
+
+
+
+
+                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+       
+
+    );
+}
+
+export default AnimalPage;
